b0e0c028c58c823fe2bcfc783c167c3578c4ebbb
BenMoreira <bmoreira@ggc.edu>
2023-05-18 17:16:30 -0400

    Fixes animation on Resume button.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,42 +5,40 @@ import { motion } from 'framer-motion'
 const Home = () => {
   return (
     <div className='bg-blue h-[calc(100vh-80px)]'>
-      <div className='mx-auto w-[70%] text-center'>
-        <motion.div className='text-8xl pt-14 text-red'
-          initial={{
-            opacity: 0,
+      <motion.div className='mx-auto w-[70%] flex flex-col justify-center items-center'
+        initial={{
+          opacity: 0
+        }}
+        animate={{
+          opacity: 1,
+          duration: 1
+        }}
+      >
+        <div className='text-8xl pt-14 text-red'>
+          Welcome.
+        </div>
+
+        <div className='text-4xl pt-10 text-beige'>
+          My name is Benjamin Moreira.
+        </div>
+
+        <div className='text-2xl pt-10 text-beige'>
+          I'm a Software Developer that has recently graduated, excited to get into the field.
+        </div>
+
+        <motion.div className='text-2xl pt-10 text-beige'
+          whileHover={{
+            scale: 1.2
           }}
-          animate={{
-            opacity: 1,
-            transition: {
-              duration: 0.5,
-            }
+          whileTap={{
+            scale:0.8
           }}
         >
-          Welcome.
-
-          <div className='text-4xl pt-10 text-beige'>
-            My name is Benjamin Moreira.
-          </div>
-
-          <div className='text-2xl pt-10 text-beige'>
-            I'm a Software Developer that has recently graduated, excited to get into the field.
-          </div>
-
-          <motion.div className='text-2xl pt-10 text-beige'
-            whileHover={{
-              scale: 1.2
-            }}
-            whileTap={{
-              scale:0.8
-            }}
-          >
-            <a href={Pdf} className='border-2 border-beige rounded-lg px-5 py-2 hover:text-red hover:border-red'>
-              Resume
-            </a>
-          </motion.div>
+          <a href={Pdf} className='border-2 border-beige rounded-lg px-5 py-2 hover:text-red hover:border-red'>
+            Resume
+          </a>
         </motion.div>
-      </div>
+      </motion.div>
     </div>
   )
 }
